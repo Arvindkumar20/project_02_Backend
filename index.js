205a6bf2bd95f871fb039cbb1e09ac4a6f6b401f
arvindkumar20 <arvindkumar8960905167@gmail.com>
2024-09-27 12:51:54 +0530

    fixed some new errors

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join('public')));
 app.use("/api/places", placesRoutes);
 app.use("/api/users", userRoutes);
 
-app.get('/',(req,res)=>{
+app.use((req,res)=>{
     res.sendFile(path.resolve(__dirname,'public','index.html'));
 
 });
