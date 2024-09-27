bdcc8eecf543960206e6623650287d579f66add0
arvindkumar20 <arvindkumar8960905167@gmail.com>
2024-09-27 11:36:14 +0530

    frontend added as public folder

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import "dotenv/config"
 // fies import 
 import placesRoutes from "./routes/places-route.js";
 import userRoutes from "./routes/users-route.js";
-import HttpError from "./models/http-error.js";
+// import HttpError from "./models/http-error.js";
 
 // variables 
 const port = process.env.PORT||4000;
@@ -19,20 +19,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.urlencoded({ extended: true }));
 app.use('/uploads/images',express.static(path.join('uploads', 'images')));
-app.use((req,res,next)=>{
-res.setHeader('Access-Control-Allow-Origin','*');
-res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
-res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE');
-next();
-});
+app.use(express.static(path.join('public')));
+
+// app.use((req,res,next)=>{
+// res.setHeader('Access-Control-Allow-Origin','*');
+// res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+// res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE');
+// next();
+// });
 
 // routes
 app.use("/api/places", placesRoutes);
 app.use("/api/users", userRoutes);
+// app.use(express.static(path.join('public')));
 
 app.use((req,res,next)=>{
-    const error=new HttpError("Could not find this route",404);
-throw error;
+    res.sendFile(path.resolve(__dirname,'public','index.html'));
+//     const error=new HttpError("Could not find this route",404);
+// throw error;
 });
 app.use((error, req, res, next) => {
     if(req.file){
